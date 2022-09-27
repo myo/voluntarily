5ba40536cb2371731bf36fea0aadf0d8d3aea7ab
myo <91497652+myo@users.noreply.github.com>
2022-09-27 03:28:00 +0300

    fix: forgot to return jwt

--- server/middleware/jwt.ts
+++ server/middleware/jwt.ts
@@ -1,6 +1,6 @@
 
 import jwt from "jsonwebtoken";
 
-export const bakeJWT = (secret: Object) => {
-    jwt.sign(secret, process.env.JWT_SECRET || "", {expiresIn: process.env.JWT_LIFE})
+export const bakeJWT = (secret: Object): string => {
+    return jwt.sign(secret, process.env.JWT_SECRET || "", {expiresIn: process.env.JWT_LIFE})
 };
